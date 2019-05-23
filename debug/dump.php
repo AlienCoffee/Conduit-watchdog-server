93bfebf8782af19b2879c564aef7203d43e05075
guapstudent1 <al.pijcov@gmail.com>
2019-05-23 17:59:18 +0500

    Убрал директорию

--- debug/dump.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-// Сохранение с форматированием
-$str='{"windows":{"tasklist":"some_script2.cmd"},"linux":{"tasklist":"some_other.sh"}}';
-echo "<pre>";
-echo json_encode(json_decode($str, true), JSON_PRETTY_PRINT);
-echo "</pre>";
